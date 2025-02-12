d1876f5995cac390fdd655599285b125511eeac5
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2025-02-12 13:16:33 -0800

    Update header: center nav items, replace text with logo, remove login buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
+import screenscoutLogo from "../assets/screenscout1.png";
 
 const Header = () => {
   const pathname = useLocation();
@@ -35,8 +36,12 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block xl:mr-8 text-2xl font-bold text-white" href="#hero">
-          ScreenScout
+        <a className="block xl:mr-8" href="#hero">
+          <img 
+            src={screenscoutLogo} 
+            alt="ScreenScout" 
+            className="h-12 md:h-14"
+          />
         </a>
 
         <nav
@@ -44,7 +49,7 @@ const Header = () => {
             openNavigation ? "flex" : "hidden"
           } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
-          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row lg:w-full lg:justify-center">
             {navigation.map((item) => (
               <a
                 key={item.id}
@@ -65,6 +70,14 @@ const Header = () => {
 
           <HamburgerMenu />
         </nav>
+
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
+        </Button>
       </div>
     </div>
   );
